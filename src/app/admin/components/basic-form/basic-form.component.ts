f8d7139c1f15cb2e2c1c8651f43647f20093cf06
Martín <huo_mmram@hotmail.com>
2023-07-06 14:41:57 -0300

    Clase 6

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
-  styleUrls: ['./basic-form.component.scss']
+  styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent implements OnInit {
-
-  constructor() { }
+  nameField = new FormControl('soy un control');
+  constructor() {}
 
   ngOnInit(): void {
+    this.nameField.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
   }
 
+  getNameValue() {
+    console.log(this.nameField.value);
+  }
 }
