23cd3f35b69ea20d82679aaaf9b5b6baf161aea6
mmramire <huo_mmram@hotmail.com>
2023-07-11 15:54:37 -0300

    Clase 22

--- src/app/admin/categories/components/categories/categories.component.ts
+++ src/app/admin/categories/components/categories/categories.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/core/models/category.model';
+import { CategoriesService } from 'src/app/core/services/categories.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
+    this.categoriesService
+      .getAllCategories()
+      .subscribe((data) => (this.categories = data));
   }
-
 }
