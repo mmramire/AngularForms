b2937dcb16618dbe67d856cc79d3e75148fd990e
mmramire <huo_mmram@hotmail.com>
2023-07-11 09:45:07 -0300

    Clase 16 a 20

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -3,14 +3,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from './../../../core/services/auth.service';
+import { MyValidators } from 'src/app/utils/validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
@@ -21,25 +21,60 @@ export class RegisterComponent implements OnInit {
     this.buildForm();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   register(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const value = this.form.value;
-      this.authService.createUser(value.email, value.password)
-      .then(() => {
+      this.authService.createUser(value.email, value.password).then(() => {
         this.router.navigate(['/auth/login']);
       });
     }
   }
 
   private buildForm() {
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
+    this.form = this.formBuilder.group(
+      {
+        email: ['', [Validators.required]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            MyValidators.validPassword,
+          ], //Validación personalizada
+        ],
+        confirmPassword: [
+          '',
+          [Validators.required], //Validación personalizada
+        ],
+        // Para validaciones condicionadas (cambian dinámicamente en con la acción del usuario en la UI)
+        type: ['company', [Validators.required]],
+        companyName: ['', [Validators.required]],
+      },
+      { validators: MyValidators.matchPassword } //Mi validación grupal, es un objeto diferente que se pasa al formGroup.
+    );
+
+    //este sería el escuchar campos del formulario y reaccionar dinamicamente
+    this.typeField.valueChanges.subscribe((value) => {
+      // seteo o no mis validadores para un formControl en particular
+      if (value === 'company') {
+        this.companyNameField.setValidators([Validators.required]);
+      } else {
+        this.companyNameField.setValidators(null);
+      }
+      // actualizo forzadamente el estado del formControl
+      this.companyNameField.updateValueAndValidity();
     });
   }
 
+  //Getters de formControl
+  get typeField() {
+    return this.form.get('type');
+  }
+
+  get companyNameField() {
+    return this.form.get('companyName');
+  }
 }
--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -1,14 +1,41 @@
 import { AbstractControl } from '@angular/forms';
 
 export class MyValidators {
-
   static isPriceValid(control: AbstractControl) {
     const value = control.value;
     console.log(value);
     if (value > 10000) {
-      return {price_invalid: true};
+      return { price_invalid: true };
     }
     return null;
   }
 
+  static matchPassword(control: AbstractControl) {
+    // control recibe todos los campos del formulario.
+    const password = control.get('password').value;
+    const confirmPassword = control.get('confirmPassword').value;
+
+    if (password === confirmPassword) {
+      return null;
+    }
+    // si hay error lo mandamos como un objeto clave valor
+    return { match_password: true };
+  }
+
+  static validPassword(control: AbstractControl) {
+    const value = control.value;
+    // En una validación Angular dice que si está todo OK debe retornar NULL
+    if (!containsNumber(value)) {
+      return { invalid_password: true };
+    }
+    return null;
+  }
+}
+
+function containsNumber(value: string) {
+  return value.split('').find((v) => isNumber(v)) !== undefined;
+}
+
+function isNumber(value: string) {
+  return !isNaN(parseInt(value, 10));
 }
