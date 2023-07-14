4c3aacb6b0900867566f46abe3e9b3093a678085
mmramire <huo_mmram@hotmail.com>
2023-07-14 15:02:20 -0300

    Clase 31

--- src/app/admin/products/components/product-edit/product-edit.component.ts
+++ src/app/admin/products/components/product-edit/product-edit.component.ts
@@ -17,6 +17,14 @@ export class ProductEditComponent implements OnInit {
   id: string;
   categories: Category[] = [];
 
+  // Para la parte select dinámico - trabajando con objetos.
+  states = [
+    { name: 'La Plata', abbrev: 'LP' },
+    { name: 'Mar del Plata', abbrev: 'MDQ' },
+    { name: 'Capital Federal', abbrev: 'C.A.B.A.' },
+    { name: 'Neuquén', abbrev: 'Neu' },
+  ];
+
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
@@ -32,7 +40,9 @@ export class ProductEditComponent implements OnInit {
     this.activatedRoute.params.subscribe((params: Params) => {
       this.id = params.id;
       this.productsService.getProduct(this.id).subscribe((product) => {
-        this.form.patchValue(product);
+        // this.form.patchValue(product);
+        // Para simular el backend con state lo interceptamos y le agregamos por defecto uno del arrary
+        this.form.patchValue({ ...product, state: this.states[1] });
         this.categoryIdField.setValue(product?.category.id);
       });
     });
@@ -42,12 +52,13 @@ export class ProductEditComponent implements OnInit {
     event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
-      this.productsService
-        .updateProduct(this.id, product)
-        .subscribe((newProduct) => {
-          console.log(newProduct);
-          this.router.navigate(['./admin/products']);
-        });
+      console.log('update -> ' + JSON.stringify(product));
+      // this.productsService
+      //   .updateProduct(this.id, product)
+      //   .subscribe((newProduct) => {
+      //     console.log(newProduct);
+      //     this.router.navigate(['./admin/products']);
+      //   });
     }
   }
 
@@ -59,6 +70,7 @@ export class ProductEditComponent implements OnInit {
       images: [''],
       description: ['', [Validators.required]],
       categoryId: ['', [Validators.required]],
+      state: ['', [Validators.required]],
     });
   }
 
